af71d3ecc0491714fa574eeb31371a0dabe329d7
podlaw <36234864+podlaw@users.noreply.github.com>
2021-11-07 04:39:12 +0300

    Add files via upload

---
+++ sums.py
@@ -0,0 +1,5 @@
+def sums(array: list) -> int:
+    """Находит количество различных сумм вида K1*A1 + K2*A2 + ... + KN*AN
+    :param array: массив целых чисел, содержащий значений A1,A2...
+    """
+    return quantity
